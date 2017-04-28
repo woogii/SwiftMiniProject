d96c12d1d0e52647c5e3b58d826d364339f3e2e9
woogii <jhsw0201@naver.com>
2017-04-28 23:56:56 +0900

    feat : Implement url request by using Flickr API

---
+++ Project08_PullToRefresh/Project08_PullToRefresh/Constants.swift
@@ -0,0 +1,53 @@
+//
+//  Constants.swift
+//  Project08_PullToRefresh
+//
+//  Created by siwook on 2017. 4. 28..
+//  Copyright © 2017년 siwook. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Constants
+
+struct Constants {
+  
+  // MARK: Flickr
+  struct Flickr {
+    static let APIBaseURL = "https://api.flickr.com/services/rest/"
+  }
+  
+  // MARK: Flickr Parameter Keys
+  struct FlickrParameterKeys {
+    static let Method = "method"
+    static let APIKey = "api_key"
+    static let GalleryID = "gallery_id"
+    static let Extras = "extras"
+    static let Format = "format"
+    static let NoJSONCallback = "nojsoncallback"
+  }
+  
+  // MARK: Flickr Parameter Values
+  struct FlickrParameterValues {
+    static let ResponseFormat = "json"
+    static let DisableJSONCallback = "1" /* 1 means "yes" */
+    //static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
+    static let PhotosSearchMethod = "flickr.photos.search"
+    static let GalleryID = "66911286-72157647613641977"
+    static let MediumURL = "url_m"
+  }
+  
+  // MARK: Flickr Response Keys
+  struct FlickrResponseKeys {
+    static let Status = "stat"
+    static let Photos = "photos"
+    static let Photo = "photo"
+    static let Title = "title"
+    static let MediumURL = "url_m"
+  }
+  
+  // MARK: Flickr Response Values
+  struct FlickrResponseValues {
+    static let OKStatus = "ok"
+  }
+}
---
+++ Project08_PullToRefresh/Project08_PullToRefresh/CustomCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  CustomCollectionViewCell.swift
+//  Project08_PullToRefresh
+//
+//  Created by siwook on 2017. 4. 28..
+//  Copyright © 2017년 siwook. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+  
+  // MARK : - Property
+  
+  @IBOutlet weak var backgroundImageView: UIImageView!
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var creatorLabel: UILabel!
+  @IBOutlet weak var contentBackgroundView: UIView!
+  @IBOutlet weak var likeIconImageView: UIImageView!
+  @IBOutlet weak var numberOfLikesLabel: UILabel!
+  @IBOutlet weak var timeInfoLabel: UILabel!
+  
+  var taskToCancelifCellIsReused: URLSessionTask? {
+    
+    didSet {
+      if let taskToCancel = oldValue {
+        taskToCancel.cancel()
+      }
+    }
+  }
+  
+  // MARK : - Nib File Loading
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    applyCornerRadiusToBackgroundImageView()
+    applyCornerRadiusToProfileImageView()
+    configureBackgroundView()
+    changeLikeIconImageViewColor()
+  }
+  
+  func changeLikeIconImageViewColor() {
+    likeIconImageView.image? = (likeIconImageView.image?.withRenderingMode(.alwaysTemplate))!
+    likeIconImageView.tintColor = UIColor.gray
+  }
+  
+  func configureBackgroundView() {
+    
+    contentBackgroundView.backgroundColor = UIColor.white
+    contentBackgroundView.layer.cornerRadius = 4
+    contentBackgroundView.layer.masksToBounds = false
+    contentBackgroundView.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+    contentBackgroundView.layer.shadowOffset = CGSize(width: 0, height: 0)
+    contentBackgroundView.layer.shadowOpacity = 0.8
+  }
+  
+  
+  func applyCornerRadiusToBackgroundImageView() {
+    backgroundImageView.layer.cornerRadius = 4
+    backgroundImageView.layer.masksToBounds = true
+  }
+  
+  func applyCornerRadiusToProfileImageView() {
+    //profileImageView.layer.cornerRadius = Constants.ImageListCollectionViewCell.ProfileImageViewCornerRadius
+    profileImageView.layer.masksToBounds = true
+  }
+
+  
+}
