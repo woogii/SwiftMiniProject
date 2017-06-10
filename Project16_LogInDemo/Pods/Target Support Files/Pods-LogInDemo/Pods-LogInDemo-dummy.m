3efdebf815b2b7d7bf2aef58c1e8864736dc9dae
woogii <jhsw0201@naver.com>
2017-06-10 22:23:00 +0900

    chore : Install Firebase pod

---
+++ Project16_LogInDemo/Pods/Target Support Files/Pods-LogInDemo/Pods-LogInDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LogInDemo : NSObject
+@end
+@implementation PodsDummy_Pods_LogInDemo
+@end
---
+++ Project16_LogInDemo/Pods/Target Support Files/Pods-LogInDemo/Pods-LogInDemo-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LogInDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LogInDemoVersionString[];
+
