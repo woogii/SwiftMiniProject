ae2c61bf5e281ecb3ea9807a467d6259008cde14
woogii <jhsw0201@naver.com>
2017-08-06 13:20:24 +0900

    feat : Apply scale animation to UIView

--- Project20_UIViewAnimation/UIViewAnimation/ViewController.swift
+++ Project20_UIViewAnimation/UIViewAnimation/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
+// MARK : - ViewController: UIViewController
+
 class ViewController: UIViewController {
 
   // 좋음 : 하트 , 보통 : 스마일, 나쁨 : 찡그림, 매우나쁨 : 악마
+  
+  // MARK : - Property 
+  
   @IBOutlet weak var faceView: FaceView!
+  let secondFace = FaceView()
+  
+  // MARK : - View Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     faceView.lineWidth = 10
     
-    
+    secondFace.frame = CGRect(x: self.view.center.x - self.faceView.frame.size.width/2, y: 350, width: 160, height: 140)
+    secondFace.backgroundColor = UIColor.white
+    view.addSubview(secondFace)
+    secondFace.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
   }
 
   override func viewDidAppear(_ animated: Bool) {
@@ -31,6 +43,18 @@ class ViewController: UIViewController {
       
       //print(self.faceView.frame)
     }, completion: nil)
+    
+    
+    UIView.animate(withDuration: 0.7, animations: {
+      self.secondFace.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+    }) { (finished) in
+      
+      UIView.animate(withDuration: 0.4, animations: {
+        self.secondFace.transform = CGAffineTransform.identity
+      })
+    }
+    
+
     
   }
   
