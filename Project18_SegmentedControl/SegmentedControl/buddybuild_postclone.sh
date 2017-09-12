e4adfa851535db5924daa6c4300edd8006587a56
woogii <jhsw0201@naver.com>
2017-09-12 19:27:07 +0200

    fix : Correct the buddybuild postclone script

---
+++ Project18_SegmentedControl/SegmentedControl/buddybuild_postclone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv ${BUDDYBUILD_SECURE_FILES}/Secret.swift Project18_SegmentedControl/SegmentedControl/Secret.swift
+
+
--- Project18_SegmentedControl/buddybuild_postclone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mv ${BUDDYBUILD_SECURE_FILES}/Secret.swift Project18_SegmentedControl/Secret.swift 
-
-
